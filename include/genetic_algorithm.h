ed9bbce3b589ce779ab33256c1e83a81470a3639
Barry Warnock <brrwarnock@gmail.com>
2017-04-06 12:26:46 -0700

    changed up h files

--- include/genetic_algorithm.h
+++ include/genetic_algorithm.h
@@ -3,7 +3,8 @@
 
 #include <tuple>
 #include <vector>
-
+#include <functional>
+#include "neural_net_genome.h"
 /**
  * elite + mutation + crossover must equal 100
  * @param population the number of genomes per population
@@ -11,8 +12,12 @@
  * @param elite the percentage of the population that should move forward unchanged (where more fit genomes have a higher chance of moving on)
  * @param mutation the percentage of the population that should be mutated and moved forward
  * @param crossover the percentage of the population that should be created through crossover
+ * @param fitness_callback a lambda that when given a pointer to a Neural_Net_Genome returns its fitness
+ * @param topology a vector representing the desired topology for the genomes
  * @return a vector of tuples representing the (best, average, worst) fitnesses per generation
  */
 std::vector<std::tuple<double, double, double>> run_genetic_algorithm(int population, int generations,
-                                                                      int elite, int mutation, int crossover);
+                                                                      int elite, int mutation, int crossover,
+                                                                      std::function<double(Neural_Net_Genome*)> fitness_callback,
+                                                                      std::vector<int> topology);
 #endif
--- include/neural_net_genome.h
+++ include/neural_net_genome.h
@@ -3,19 +3,24 @@
 #include <vector>
 #include "neural_net.h"
 #include "stochastic_back_propogation.h"
-#include "nng_pool.h"
 
 class Neural_Net_Genome {
 protected:
+    static std::vector<Neural_Net_Genome*> freeNNGs;//used to prevent unnecessary reallocations of memory
     std::vector<int> topology;
     std::vector<double> weights;//genes
     NN_Data_Set data;//used to check fitness
 public:
+    //pooling functions
+    static Neural_Net_Genome* get_NNG(std::vector<int> topology); //either return a NNG if one is already available or create a new one then return it
+    static void return_NNG(Neural_Net_Genome* nng); //return a NNG to the pool
+    static void cleanup(); //free all of the NNGs in the free_pool
+    //end of pooling
+
     Neural_Net_Genome(std::vector<int> topology, NN_Data_Set data);
     void init(std::vector<int> topology, NN_Data_Set data);
     Neural_Net get_as_net();
-    double fitness();
-    Neural_Net_Genome* mutate(NNG_Pool pool);
-    Neural_Net_Genome* crossover(NNG_Pool pool);
+    Neural_Net_Genome* mutate();
+    Neural_Net_Genome* crossover(Neural_Net_Genome*);
 };
 #endif
